8c14bd750f41794186694ec68e1485143103aa0b
Astro Han <astrohan@Astroui-iMac.local>
2024-09-07 16:00:10 +0900

    ✨ feat: supabase와 연동 설정

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,29 @@
-<main>
-  <h1>Vite + Svelte</h1>
-</main>
+<script lang="ts">
+  import { supabase } from "./lib/supabase";
+  import type { User } from "./types/users";
+  let users: User[] = [];
+
+  const loadUsers = async () => {
+    const { data, error } = await supabase.from("users").select("*");
+    console.log(data);
+    if (error) {
+      console.error("Error fetching users:", error);
+    } else {
+      users = data;
+    }
+  };
+
+  loadUsers();
+</script>
+
+<h1>User List</h1>
+
+{#if users.length > 0}
+  <ul>
+    {#each users as user}
+      <li>{user.name}</li>
+    {/each}
+  </ul>
+{:else}
+  <p>No users found</p>
+{/if}
